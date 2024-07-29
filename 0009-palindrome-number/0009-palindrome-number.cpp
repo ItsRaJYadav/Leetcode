22e8a4bec1d28307f1ee6f95b320dccbc52b0df1
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2024-07-29 23:16:55 +0530

    Time: 12 ms (33.49%), Space: 5.9 MB (100%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;  // base condition
+        
+        string s = to_string(x); 
+        string s2 = s; 
+        reverse(s2.begin(), s2.end());
+        return (s == s2)?1:0;
+        
+    }
+};
