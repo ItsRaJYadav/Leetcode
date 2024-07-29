653c1d70f060d9251bd70bd8c3d3526447aafa9f
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2024-07-29 23:08:05 +0530

    Time: 3 ms (40.56%), Space: 5.9 MB (100%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        std::string numStr = std::to_string(num);
-
-    // Find and replace the first occurrence of '6' with '9'
-    for (char &c : numStr) {
-        if (c == '6') {
-            c = '9'; // Replace the first '6' with '9'
-            break;   // Only change the first occurrence
+        string s = to_string(num);
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '6'){
+            s[i] = '9';
+            break;
         }
     }
-    return std::stoi(numStr);
+    return stoi(s); //conversion
     }
 };
