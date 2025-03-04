939fd913327480c908189401d49c42c58aac0c4f
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-03-05 00:29:57 +0530

    Time: 0 ms (100%), Space: 29.5 MB (59.08%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,6 +5,9 @@ public:
         k = k % n;
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
+        // for(int i=0;i<nums.size();i++){
+        //     cout<<nums[i]<< " ";
+        // }
         reverse(nums.begin() + k, nums.end());
     }
 };
