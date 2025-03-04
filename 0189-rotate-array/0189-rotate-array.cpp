39eac7e0df76d7f6e11ad3e0b69632446b7e7340
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-03-05 00:18:34 +0530

    Time: 0 ms (100%), Space: 29.4 MB (79.96%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> temp(nums.size());
-        for(int i =0; i<nums.size(); i++){
-            temp[(i+k)% nums.size()]= nums[i];    // if we store the value in same array then it                                                      will overwrite it
-        }
-        nums=temp;
-        
-        
+        int n = nums.size();
+        k = k % n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
