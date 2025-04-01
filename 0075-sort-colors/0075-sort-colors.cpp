d03284f42bd0427f43f2c756c40c975fa3da7d64
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-04-01 23:39:02 +0530

    Time: 0 ms (100%), Space: 11.6 MB (74.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         int low=0, high=nums.size()-1,mid=0;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[mid],nums[low]);
-                mid++;low++;
-            }
-            else if(nums[mid]==1){
-                
+        // int one=0, two=0, zero=0;
+        // for(int i=0;i<nums.size(); i++){
+        //     if(nums[i]==0){
+        //        zero++;
+        //     }
+        //     else if(nums[i]==1){
+        //         one++;
+        //     }else{
+        //         two++;
+        //     }
+        // }
+
+        // for(int i=0; i<zero; i++){
+        //     nums[i]=0;
+        // }
+        // for(int i=zero; i<zero+one; i++){
+        //     nums[i]=1;
+        // }
+        // for(int i=(zero+one); i<nums.size(); i++){
+        //     nums[i]=2;
+        // }
+
+        int low = 0, high = nums.size() - 1, mid = 0;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
                 mid++;
-            }
-            else{
-                swap(nums[mid],nums[high]);
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+
                 high--;
             }
         }
