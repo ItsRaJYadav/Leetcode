87a0a9501a1628271fe81006123101dff820f31f
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-04-11 00:19:58 +0530

    Time: 1 ms (16.36%), Space: 8.6 MB (96.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string x) {
+         stack<char> s;
+     for(int i=0; i<x.length(); i++) {
+         
+         char ch =x[i];
+         
+         //if opening bracket, stack push
+         //if close bracket, stacktop check and pop
+         
+         if(ch == '(' || ch == '{' || ch == '['){
+             s.push(ch);
+         }
+         else
+         {
+             //for closing bracket
+             if(!s.empty()) {
+                  char top = s.top();
+                  if( (ch == ')' && top == '(') || 
+                     ( ch == '}' && top == '{') || 
+                     (ch == ']' && top == '[') ) {
+                      s.pop();
+                  }
+                 else
+                 {
+                     return false;
+                 }
+             }
+             else
+             {
+                 return false;
+             } 
+         }  
+     }
+    
+    if(s.empty())
+        return true;
+    else
+        return false;
+        
+    }
+};
