56b7b8be64dbb560001936f8e7a17743e7daf04f
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-05-27 17:56:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (83.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    bool isValid(string x) {
-         stack<char> s;
-     for(int i=0; i<x.length(); i++) {
-         
-         char ch =x[i];
-         
-         //if opening bracket, stack push
-         //if close bracket, stacktop check and pop
-         
-         if(ch == '(' || ch == '{' || ch == '['){
-             s.push(ch);
-         }
-         else
-         {
-             //for closing bracket
-             if(!s.empty()) {
-                  char top = s.top();
-                  if( (ch == ')' && top == '(') || 
-                     ( ch == '}' && top == '{') || 
-                     (ch == ']' && top == '[') ) {
-                      s.pop();
-                  }
-                 else
-                 {
-                     return false;
-                 }
-             }
-             else
-             {
-                 return false;
-             } 
-         }  
-     }
-    
-    if(s.empty())
-        return true;
-    else
-        return false;
-        
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]== '(' || s[i]=='{' || s[i]== '['){
+                st.push(s[i]);
+            }
+            else{
+                if (st.empty()) return false;
+                char top = st.top();
+                if(
+                   s[i]== ')'  && top!= '(' ||
+                   s[i]=='}' && top != '{' ||
+                   s[i]== ']' && top!= '['
+                ){
+                    return false;
+                }
+                st.pop();
+            }
+
+            
+        }
+
+        return st.empty();
     }
 };
