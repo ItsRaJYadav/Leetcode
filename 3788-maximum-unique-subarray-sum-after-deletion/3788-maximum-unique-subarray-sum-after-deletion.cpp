6487d13d69c569ec4f3b396ce6721041f982434c
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-07-26 13:26:52 +0530

    Time: 3 ms (24.59%), Space: 29.7 MB (38.12%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int max_sum = 0;
+        unordered_set<int> seen;
+
+        bool onlyNeg = true;
+
+        int maxNeg = INT_MIN;
+
+        for (int num : nums) {
+            if (num >= 0 && seen.find(num) == seen.end()) {
+                max_sum += num;
+                seen.insert(num);
+                onlyNeg = false;
+            } else {
+                maxNeg = max(maxNeg, num);
+            }
+        }
+
+        return onlyNeg== true? maxNeg: max_sum;
+    }
+};
