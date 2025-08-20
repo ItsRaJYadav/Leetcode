0cf7f8ff1d6545dafc293f03ad0b912c802fc19d
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2025-08-20 16:23:16 +0530

    Time: 0 ms (100%), Space: 18.3 MB (17.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target)
-    {
-        int n = nums.size();
-        int start = -1;
-        int end = -1;
-        
-       
-
-        for(int i=0; i<nums.size();i++){
-            if(nums[i]== target){
-                if(start== -1)start= i;
-                end= i;
+    int bs(vector<int>nums, int t, bool ff){
+         int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        int res= -1;
+        while(start<=end){
+            int mid= start+ (end-start)/2;
+            if(nums[mid]==t){
+               res= mid;
+               if(ff){
+                end= mid-1;
+               }else{
+                start= mid+1;
+               }
+            }else if(nums[mid]<t){
+                start= mid+1;
+               
+            } else{
+                 end= mid-1;
             }
         }
-    return {start,end};
+
+        return res;
+    }
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
+       
+
+        int first = bs(nums, target, true);
+        int second= bs(nums, target, false);
+
+        // for(int i=0; i<nums.size();i++){
+        //     if(nums[i]== target){
+        //         if(start== -1)start= i;
+        //         end= i;
+        //     }
+        // }
+    return {first,second};
     }
 };
